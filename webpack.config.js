8f1e82cf6b799f582326cbea1c7501ad4d38ac3f
Chet Corcos <ccorcos@gmail.com>
2017-03-07 22:47:44 -0800

    webpack built to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './lib/js/src/index.js',
   },
   output: {
-    path: path.join(__dirname, "src"),
+    path: path.join(__dirname, "dist"),
     filename: '[name].js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    })
+  ]
 };
